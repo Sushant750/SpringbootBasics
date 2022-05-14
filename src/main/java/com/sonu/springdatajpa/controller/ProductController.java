dd8e70e25527603a3a5da34f7f4cf41fe66de74a
Sushant750 <sonukumaroffc@gmail.com>
2022-05-14 07:44:24 -0700

    Baiscs_of_logs_and_profiling

--- src/main/java/com/sonu/springdatajpa/controller/ProductController.java
+++ src/main/java/com/sonu/springdatajpa/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.sonu.springdatajpa.controller;
 
 import com.sonu.springdatajpa.entities.Product;
 import com.sonu.springdatajpa.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,9 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductController.class);
+
     @GetMapping("/products/")
     public List<Product> getListOfProducts(){
         return productRepository.findAll();
@@ -20,21 +25,25 @@ public class ProductController {
 
     @GetMapping("/products/{id}")
     public Product getProduct(@PathVariable("id") int id){
+        LOG.info("Finding Product on basis of id {}", id);
         return productRepository.findById(id).get();
     }
 
     @PostMapping("/products/details/")
     public Product createProduct(@RequestBody Product product){
+        LOG.info("Adding product details with id{}", product.getId());
         return productRepository.save(product);
     }
 
     @PutMapping("/products/")
     public Product updateProduct(@RequestBody Product product){
+        LOG.info("Updating product details with id{}", product.getId());
         return productRepository.save(product);
     }
 
     @DeleteMapping("/products/{id}")
     public void deleteProduct(@PathVariable("id") int id){
+        LOG.info("Product got deleted with id{}", id);
         productRepository.deleteById(id);
     }
 }
