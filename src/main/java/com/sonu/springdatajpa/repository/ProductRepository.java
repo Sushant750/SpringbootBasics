ca54a6fa9e91f6f255e95a05eebd2c50dfd8144d
Sushant750 <sonukumaroffc@gmail.com>
2022-05-14 23:36:38 -0700

    Spacesadded

--- src/main/java/com/sonu/springdatajpa/repository/ProductRepository.java
+++ src/main/java/com/sonu/springdatajpa/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.sonu.springdatajpa.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
